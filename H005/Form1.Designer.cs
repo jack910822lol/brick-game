8b723bba98a336e76884d4180fe112ee42390183
jack910822lol <32065912+jack910822lol@users.noreply.github.com>
2022-01-13 16:24:48 +0800

    :D
    d

---
+++ H005/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace H005
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.red = new System.Windows.Forms.PictureBox();
+            this.blue = new System.Windows.Forms.PictureBox();
+            this.green = new System.Windows.Forms.PictureBox();
+            this.Obrick = new System.Windows.Forms.PictureBox();
+            this.board = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obrick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "分數 :";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(412, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "生命值 : ";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(173, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 111);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // red
+            // 
+            this.red.Image = global::H005.Properties.Resources.redbrick;
+            this.red.Location = new System.Drawing.Point(584, 602);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(43, 27);
+            this.red.TabIndex = 8;
+            this.red.TabStop = false;
+            this.red.Visible = false;
+            // 
+            // blue
+            // 
+            this.blue.Image = global::H005.Properties.Resources.bluebrick;
+            this.blue.Location = new System.Drawing.Point(652, 602);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(43, 27);
+            this.blue.TabIndex = 7;
+            this.blue.TabStop = false;
+            this.blue.Visible = false;
+            // 
+            // green
+            // 
+            this.green.Image = global::H005.Properties.Resources.greenbrick;
+            this.green.Location = new System.Drawing.Point(520, 602);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(43, 27);
+            this.green.TabIndex = 6;
+            this.green.TabStop = false;
+            this.green.Visible = false;
+            // 
+            // Obrick
+            // 
+            this.Obrick.Image = global::H005.Properties.Resources._0215;
+            this.Obrick.Location = new System.Drawing.Point(451, 602);
+            this.Obrick.Name = "Obrick";
+            this.Obrick.Size = new System.Drawing.Size(43, 27);
+            this.Obrick.TabIndex = 3;
+            this.Obrick.TabStop = false;
+            this.Obrick.Visible = false;
+            // 
+            // board
+            // 
+            this.board.Image = global::H005.Properties.Resources.png_0937;
+            this.board.Location = new System.Drawing.Point(243, 626);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(98, 19);
+            this.board.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.board.TabIndex = 2;
+            this.board.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.Image = global::H005.Properties.Resources.png_0803;
+            this.ball.Location = new System.Drawing.Point(273, 587);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(31, 33);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 120);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1126, 671);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Obrick);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Obrick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox board;
+        private System.Windows.Forms.PictureBox Obrick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox green;
+        private System.Windows.Forms.PictureBox blue;
+        private System.Windows.Forms.PictureBox red;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ H005/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace H005
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer player1 = new WMPLib.WindowsMediaPlayer();
+        SoundPlayer player2 = new SoundPlayer("noo.wav");
+
+        int score = 0;
+        int life = 2;
+        bool dead = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PictureBox[] brick = new PictureBox[48];
+        int brickCounter=0;
+        int perBrickPixRow = 70, perBrickPixCol=40;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { board.Left -= 5; }
+            if (e.KeyCode == Keys.Right) { board.Left += 5; }
+        }
+
+        int leftSpeed = 7;
+        int topSpeed = -20;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label1.Text = "分數 :" + score.ToString();
+            label2.Text = "生命值 : " + life.ToString();
+            ball.Left += leftSpeed;
+            ball.Top += topSpeed;
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "block")
+                {
+                    if (ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        player2.Play();
+                        this.Controls.Remove(x);
+                        topSpeed = -topSpeed;
+                        score++;
+                    }
+                }
+            }
+            if (ball.Top<=0||ball.Bounds.IntersectsWith(board.Bounds))
+            {
+                player2.Play();
+                topSpeed = -topSpeed;
+            }
+            if(ball.Left<=0||ball.Right>=this.Width)
+            {
+                player2.Play();
+                leftSpeed = -leftSpeed;
+            }
+            if(ball.Bottom>=718)
+            {
+                this.KeyPreview = false;
+                timer1.Enabled = false;
+                life--;
+                dead = true;
+            }
+            if(dead&&life>0)
+            {
+                button1.Enabled = true;
+                button1.Visible = true;
+                button1.Text = "還有"+life+"條生命";
+                dead = false;
+            }
+            if(dead&&life==0)
+            {
+                ball.Location = new Point(board.Left, board.Top - 30);
+                this.KeyPreview = false;
+                timer1.Stop();
+                label2.Text = "遊戲結束";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ball.Location = new Point(board.Left, board.Top-30);
+            this.KeyPreview = true;
+            button1.Enabled = false;
+            button1.Visible = false;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button2.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player1.URL = "frog_song.mp3";
+
+            for (int k = 0; k < 12; k++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    brick[brickCounter] = new PictureBox();
+                    if(j==0)
+                        brick[brickCounter].Image = red.Image;
+                    else if(j==1)
+                        brick[brickCounter].Image = Obrick.Image;
+                    else if(j==2)
+                        brick[brickCounter].Image = blue.Image;
+                    else if(j==3)
+                        brick[brickCounter].Image = green.Image;
+                    brick[brickCounter].Size = new System.Drawing.Size(50, 20);
+                    brick[brickCounter].Location = new Point(k*perBrickPixRow+10, j*perBrickPixCol+10);
+                    brick[brickCounter].BringToFront();
+                    brick[brickCounter].BackColor = System.Drawing.Color.Transparent;
+                    brick[brickCounter].Tag = "block";
+                    this.Controls.Add(brick[brickCounter]);
+                    brickCounter++;
+                }
+            }
+        }
+    }
+}
